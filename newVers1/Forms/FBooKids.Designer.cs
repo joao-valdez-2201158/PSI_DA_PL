984f401435ec491913d91d3c7dc3fa1ee462aa64
Eduardo <2202420@my.ipleiria.pt>
2021-05-12 18:50:00 +0100

    Mudanças(teste)
    alteracao

--- newVers1/Forms/FBooKids.Designer.cs
+++ newVers1/Forms/FBooKids.Designer.cs
@@ -53,21 +53,18 @@ namespace Projeto_DA_BooKids.Forms
             // groupBox6
             // 
             this.groupBox6.Controls.Add(this.BTN_GestaoAnimadores);
-            this.groupBox6.Location = new System.Drawing.Point(502, 470);
-            this.groupBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox6.Location = new System.Drawing.Point(376, 382);
             this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox6.Size = new System.Drawing.Size(287, 75);
+            this.groupBox6.Size = new System.Drawing.Size(215, 61);
             this.groupBox6.TabIndex = 28;
             this.groupBox6.TabStop = false;
             this.groupBox6.Text = "Gestão de Aimadores";
             // 
             // BTN_GestaoAnimadores
             // 
-            this.BTN_GestaoAnimadores.Location = new System.Drawing.Point(11, 28);
-            this.BTN_GestaoAnimadores.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_GestaoAnimadores.Location = new System.Drawing.Point(8, 23);
             this.BTN_GestaoAnimadores.Name = "BTN_GestaoAnimadores";
-            this.BTN_GestaoAnimadores.Size = new System.Drawing.Size(268, 28);
+            this.BTN_GestaoAnimadores.Size = new System.Drawing.Size(201, 23);
             this.BTN_GestaoAnimadores.TabIndex = 8;
             this.BTN_GestaoAnimadores.Text = "Ir para a Gestão de Animadores";
             this.BTN_GestaoAnimadores.UseVisualStyleBackColor = true;
@@ -76,21 +73,18 @@ namespace Projeto_DA_BooKids.Forms
             // groupBox5
             // 
             this.groupBox5.Controls.Add(this.BTN_GestaoEscolas);
-            this.groupBox5.Location = new System.Drawing.Point(142, 470);
-            this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox5.Location = new System.Drawing.Point(106, 382);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox5.Size = new System.Drawing.Size(287, 75);
+            this.groupBox5.Size = new System.Drawing.Size(215, 61);
             this.groupBox5.TabIndex = 27;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Gestão Escolas";
             // 
             // BTN_GestaoEscolas
             // 
-            this.BTN_GestaoEscolas.Location = new System.Drawing.Point(8, 28);
-            this.BTN_GestaoEscolas.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_GestaoEscolas.Location = new System.Drawing.Point(6, 23);
             this.BTN_GestaoEscolas.Name = "BTN_GestaoEscolas";
-            this.BTN_GestaoEscolas.Size = new System.Drawing.Size(271, 28);
+            this.BTN_GestaoEscolas.Size = new System.Drawing.Size(203, 23);
             this.BTN_GestaoEscolas.TabIndex = 8;
             this.BTN_GestaoEscolas.Text = "Ir para a gestão de Escolas";
             this.BTN_GestaoEscolas.UseVisualStyleBackColor = true;
@@ -99,21 +93,18 @@ namespace Projeto_DA_BooKids.Forms
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.BTN_ProdutosVendidos);
-            this.groupBox4.Location = new System.Drawing.Point(502, 346);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox4.Location = new System.Drawing.Point(376, 281);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox4.Size = new System.Drawing.Size(287, 75);
+            this.groupBox4.Size = new System.Drawing.Size(215, 61);
             this.groupBox4.TabIndex = 26;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Gestão Produtos Vendidos";
             // 
             // BTN_ProdutosVendidos
             // 
-            this.BTN_ProdutosVendidos.Location = new System.Drawing.Point(11, 28);
-            this.BTN_ProdutosVendidos.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_ProdutosVendidos.Location = new System.Drawing.Point(8, 23);
             this.BTN_ProdutosVendidos.Name = "BTN_ProdutosVendidos";
-            this.BTN_ProdutosVendidos.Size = new System.Drawing.Size(268, 28);
+            this.BTN_ProdutosVendidos.Size = new System.Drawing.Size(201, 23);
             this.BTN_ProdutosVendidos.TabIndex = 8;
             this.BTN_ProdutosVendidos.Text = "Ir para  gestão de Produtos Vendidos";
             this.BTN_ProdutosVendidos.UseVisualStyleBackColor = true;
@@ -122,21 +113,18 @@ namespace Projeto_DA_BooKids.Forms
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.BTN_GestaoEventos);
-            this.groupBox3.Location = new System.Drawing.Point(142, 346);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Location = new System.Drawing.Point(106, 281);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox3.Size = new System.Drawing.Size(287, 75);
+            this.groupBox3.Size = new System.Drawing.Size(215, 61);
             this.groupBox3.TabIndex = 24;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Gestão Eventos";
             // 
             // BTN_GestaoEventos
             // 
-            this.BTN_GestaoEventos.Location = new System.Drawing.Point(8, 28);
-            this.BTN_GestaoEventos.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_GestaoEventos.Location = new System.Drawing.Point(6, 23);
             this.BTN_GestaoEventos.Name = "BTN_GestaoEventos";
-            this.BTN_GestaoEventos.Size = new System.Drawing.Size(271, 28);
+            this.BTN_GestaoEventos.Size = new System.Drawing.Size(203, 23);
             this.BTN_GestaoEventos.TabIndex = 8;
             this.BTN_GestaoEventos.Text = "Ir para a gestão de Eventos";
             this.BTN_GestaoEventos.UseVisualStyleBackColor = true;
@@ -145,21 +133,18 @@ namespace Projeto_DA_BooKids.Forms
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.BTN_GestaoProdutos);
-            this.groupBox2.Location = new System.Drawing.Point(502, 234);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Location = new System.Drawing.Point(376, 190);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(289, 75);
+            this.groupBox2.Size = new System.Drawing.Size(217, 61);
             this.groupBox2.TabIndex = 25;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Gestão Vendas de Produtos";
             // 
             // BTN_GestaoProdutos
             // 
-            this.BTN_GestaoProdutos.Location = new System.Drawing.Point(11, 28);
-            this.BTN_GestaoProdutos.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_GestaoProdutos.Location = new System.Drawing.Point(8, 23);
             this.BTN_GestaoProdutos.Name = "BTN_GestaoProdutos";
-            this.BTN_GestaoProdutos.Size = new System.Drawing.Size(268, 28);
+            this.BTN_GestaoProdutos.Size = new System.Drawing.Size(201, 23);
             this.BTN_GestaoProdutos.TabIndex = 8;
             this.BTN_GestaoProdutos.Text = "Ir para a gestão de Venda de Produtos";
             this.BTN_GestaoProdutos.UseVisualStyleBackColor = true;
@@ -170,10 +155,9 @@ namespace Projeto_DA_BooKids.Forms
             this.LBBooKids.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.LBBooKids.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBBooKids.Location = new System.Drawing.Point(142, 96);
-            this.LBBooKids.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBBooKids.Location = new System.Drawing.Point(106, 78);
             this.LBBooKids.Name = "LBBooKids";
-            this.LBBooKids.Size = new System.Drawing.Size(647, 106);
+            this.LBBooKids.Size = new System.Drawing.Size(485, 86);
             this.LBBooKids.TabIndex = 22;
             this.LBBooKids.Text = "BOOKIDS";
             this.LBBooKids.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -181,21 +165,18 @@ namespace Projeto_DA_BooKids.Forms
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.BTN_GestaoClientes);
-            this.groupBox1.Location = new System.Drawing.Point(142, 234);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(106, 190);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(287, 75);
+            this.groupBox1.Size = new System.Drawing.Size(215, 61);
             this.groupBox1.TabIndex = 23;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Gestão Clientes";
             // 
             // BTN_GestaoClientes
             // 
-            this.BTN_GestaoClientes.Location = new System.Drawing.Point(8, 28);
-            this.BTN_GestaoClientes.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_GestaoClientes.Location = new System.Drawing.Point(6, 23);
             this.BTN_GestaoClientes.Name = "BTN_GestaoClientes";
-            this.BTN_GestaoClientes.Size = new System.Drawing.Size(271, 28);
+            this.BTN_GestaoClientes.Size = new System.Drawing.Size(203, 23);
             this.BTN_GestaoClientes.TabIndex = 8;
             this.BTN_GestaoClientes.Text = "Ir para a gestão de Clientes";
             this.BTN_GestaoClientes.UseVisualStyleBackColor = true;
@@ -203,9 +184,9 @@ namespace Projeto_DA_BooKids.Forms
             // 
             // FBooKids
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(919, 630);
+            this.ClientSize = new System.Drawing.Size(689, 512);
             this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
@@ -213,8 +194,9 @@ namespace Projeto_DA_BooKids.Forms
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.LBBooKids);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FBooKids";
-            this.Text = "FBooKids";
+            this.Text = "BooKids";
             this.Load += new System.EventHandler(this.FBooKids_Load);
             this.groupBox6.ResumeLayout(false);
             this.groupBox5.ResumeLayout(false);
